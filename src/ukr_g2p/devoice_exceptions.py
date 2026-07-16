637ea02bf9b1e2e51b0772dc01dfc7f5506a5ca2
yana-hrytsenko <yanagrits22@gmail.com>
2026-07-16 14:15:38 +0200

    Restore -ться -> -цця reflexive verb assimilation

--- src/ukr_g2p/devoice_exceptions.py
+++ src/ukr_g2p/devoice_exceptions.py
@@ -1,8 +1,10 @@
 import re
-from .mappings import DEVOICE_EXCEPTIONS
+from .mappings import DEVOICE_EXCEPTIONS, REFLEXIVE_ASSIMILATION
 
 
-def devoice(text):
+def exceptions(text):
     for src, tgt in DEVOICE_EXCEPTIONS.items():
         text = re.sub(rf'\b{re.escape(src)}', tgt, text)
+    for src, tgt in REFLEXIVE_ASSIMILATION.items():
+        text = re.sub(rf'{re.escape(src)}\b', tgt, text)
     return text
